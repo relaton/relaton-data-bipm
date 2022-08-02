ebd0647940f14b8790ebc6faf7794b1bb18e9506
Andrei Kislichenko <android.2net@gmail.com>
2022-08-02 17:56:43 +0200

    fix source repos absence

--- crawler.rb
+++ crawler.rb
@@ -6,8 +6,11 @@ require 'relaton_bipm'
 FileUtils.rm_rf('data')
 FileUtils.rm Dir.glob('index.*')
 
+system('git clone https://github.com/metanorma/bipm-data-outcomes bipm-data-outcomes')
+system('git clone https://github.com/metanorma/bipm-si-brochure bipm-si-brochure')
+
 RelatonBipm::DataFetcher.fetch 'bipm-data-outcomes'
 RelatonBipm::DataFetcher.fetch 'bipm-si-brochure'
 
-system("zip index.zip index.yaml")
-system("git add index.yaml index.zip")
+system('zip index.zip index.yaml')
+system('git add index.yaml index.zip')

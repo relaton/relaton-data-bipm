6c3cb8f2a73600c9bc696e682580cfb67b8092cd
Andrei Kislichenko <android.2net@gmail.com>
2022-08-03 12:20:46 +0200

    fix si-brochure compiling

--- crawler.rb
+++ crawler.rb
@@ -11,9 +11,11 @@ system('git clone https://github.com/metanorma/bipm-data-outcomes bipm-data-outc
 system('git clone https://github.com/metanorma/bipm-si-brochure bipm-si-brochure')
 
 # Generate si-brochure documents
-system('ls', chdir: 'bipm-si-brochure')
-system('bundle install', chdir: 'bipm-si-brochure')
-system('bundle exec metanorma site generate -c brochure.yml --agree-to-terms', chdir: 'bipm-si-brochure')
+Bundler.with_clean_env do
+  system('ls', chdir: 'bipm-si-brochure')
+  system('bundle install', chdir: 'bipm-si-brochure')
+  system('bundle exec metanorma site generate -c brochure.yml --agree-to-terms', chdir: 'bipm-si-brochure')
+end
 
 # Run converters
 RelatonBipm::DataFetcher.fetch 'bipm-data-outcomes'

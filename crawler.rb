6fdf2d2c16e78980f84b25b3bb870331adfafe89
Andrei Kislichenko <android.2net@gmail.com>
2023-02-16 16:17:55 -0500

    disable bipm-si-brochure

--- crawler.rb
+++ crawler.rb
@@ -9,20 +9,20 @@ FileUtils.rm Dir.glob('index.*')
 
 # Clone repositories
 system('git clone https://github.com/metanorma/bipm-data-outcomes bipm-data-outcomes')
-system('git clone https://github.com/metanorma/bipm-si-brochure bipm-si-brochure')
+# system('git clone https://github.com/metanorma/bipm-si-brochure bipm-si-brochure')
 system('git clone https://github.com/relaton/rawdata-bipm-metrologia rawdata-bipm-metrologia')
 
 # Generate si-brochure documents
-Bundler.with_unbundled_env do
-  system('ls', chdir: 'bipm-si-brochure')
-  system('bundle update', chdir: 'bipm-si-brochure')
-  system('bundle exec metanorma site generate --agree-to-terms', chdir: 'bipm-si-brochure')
-  system('ls', chdir: 'bipm-si-brochure/site/documents')
-end
+# Bundler.with_unbundled_env do
+#   system('ls', chdir: 'bipm-si-brochure')
+#   system('bundle update', chdir: 'bipm-si-brochure')
+#   system('bundle exec metanorma site generate --agree-to-terms', chdir: 'bipm-si-brochure')
+#   system('ls', chdir: 'bipm-si-brochure/site/documents')
+# end
 
 # Run converters
 RelatonBipm::DataFetcher.fetch 'bipm-data-outcomes'
-RelatonBipm::DataFetcher.fetch 'bipm-si-brochure'
+# RelatonBipm::DataFetcher.fetch 'bipm-si-brochure'
 RelatonBipm::DataFetcher.fetch 'rawdata-bipm-metrologia'
 
 # Zip index

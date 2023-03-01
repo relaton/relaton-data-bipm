886dfca9fd57ce24403fc406a00aae43effa7b9f
Andrei Kislichenko <android.2net@gmail.com>
2023-02-28 19:56:11 -0500

    fetch rawdata-bipm-metrologia & temporary disable cron

--- crawler.rb
+++ crawler.rb
@@ -8,17 +8,17 @@ FileUtils.rm_rf('data')
 FileUtils.rm Dir.glob('index.*')
 
 # Clone repositories
-system('git clone https://github.com/metanorma/bipm-data-outcomes bipm-data-outcomes')
-system('git clone https://github.com/metanorma/bipm-si-brochure bipm-si-brochure')
-system('git clone https://${RELATON_CI_PAT}@github.com/relaton/rawdata-bipm-metrologia rawdata-bipm-metrologia')
+# system('git clone https://github.com/metanorma/bipm-data-outcomes bipm-data-outcomes')
+# system('git clone https://github.com/metanorma/bipm-si-brochure bipm-si-brochure')
+# system('git clone https://${RELATON_CI_PAT}@github.com/relaton/rawdata-bipm-metrologia rawdata-bipm-metrologia')
 
-# Generate si-brochure documents
-Bundler.with_unbundled_env do
-  system('ls', chdir: 'bipm-si-brochure')
-  system('bundle update', chdir: 'bipm-si-brochure')
-  system('bundle exec metanorma site generate --agree-to-terms', chdir: 'bipm-si-brochure')
-  system('ls', chdir: 'bipm-si-brochure/site/documents')
-end
+# # Generate si-brochure documents
+# Bundler.with_unbundled_env do
+#   system('ls', chdir: 'bipm-si-brochure')
+#   system('bundle update', chdir: 'bipm-si-brochure')
+#   system('bundle exec metanorma site generate --agree-to-terms', chdir: 'bipm-si-brochure')
+#   system('ls', chdir: 'bipm-si-brochure/site/documents')
+# end
 
 # Run converters
 RelatonBipm::DataFetcher.fetch 'bipm-data-outcomes'

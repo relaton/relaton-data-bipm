3676c7bbc57a87a782835d3060b020fad8a6454d
Andrei Kislichenko <android.2net@gmail.com>
2023-11-11 23:20:47 -0500

    fix issue #5

--- crawler.rb
+++ crawler.rb
@@ -26,3 +26,13 @@ end
 RelatonBipm::DataFetcher.fetch 'bipm-data-outcomes'
 RelatonBipm::DataFetcher.fetch 'bipm-si-brochure'
 RelatonBipm::DataFetcher.fetch 'rawdata-bipm-metrologia'
+
+index_file = RelatonBipm::BipmBibliography::INDEX_FILE
+index = Relaton::Index.find_or_create :bipm, file: index_file
+Dir["static/**/*.yaml"].each do |f|
+  doc = YAML.load_file f
+  id = doc["docid"][0]["id"]
+  pubid = RelatonBipm::Id.new id
+  index.add_or_update pubid.to_hash, f
+end
+index.save

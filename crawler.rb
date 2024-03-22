03f798306d98f6b35e52a269ab2cd8f8106625a2
Andrei Kislichenko <android.2net@gmail.com>
2024-03-22 19:24:43 -0400

    update crawler, fix new Id parser usage

--- crawler.rb
+++ crawler.rb
@@ -39,7 +39,7 @@ index = Relaton::Index.find_or_create :bipm, file: index_file
 Dir["static/**/*.yaml"].each do |f|
   doc = YAML.load_file f
   id = doc["docid"][0]["id"]
-  pubid = RelatonBipm::Id.new id
+  pubid = RelatonBipm::Id.new.parse id
   index.add_or_update pubid.to_hash, f
 end
 index.save
